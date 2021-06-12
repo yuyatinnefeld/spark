7f2fa73fccd67196385b22ba8c168b893f7399f0
Yuya <yuyatinnefeld@gmail.com>
2021-06-12 15:34:07 +0200

    updated BasicDS app

--- scala/my-app/src/main/scala/book/advanced_analytics/BasicDS.scala
+++ scala/my-app/src/main/scala/book/advanced_analytics/BasicDS.scala
@@ -1,7 +1,7 @@
 package book.advanced_analytics
 
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object BasicDS {
 
@@ -26,8 +26,8 @@ object BasicDS {
       .csv("data/advanced_analytics_book")
       .as[dataName]
 
-    //dataSet.show()
-    println("total Records: "+dataSet.count())
+    println("### raw data ###")
+    dataSet.show()
 
     dataSet
       .groupBy("is_match")
@@ -35,15 +35,52 @@ object BasicDS {
       .orderBy($"count".desc)
       .show()
 
+    println("### process1: summarized ###")
     val summary = dataSet.describe()
     summary.select("summary","cmp_sex", "cmp_fname_c1", "cmp_fname_c2").show()
 
 
-    val matches = dataSet.where("is_match = true")
+    val matches = dataSet.filter($"is_match" === true) // or dataSet.where("is_match = true")
     val matchSummary = matches.describe()
 
-    val misses = dataSet.filter($"is_match === false")
+    val misses = dataSet.filter($"is_match" === false)
     val missSummary = misses.describe()
+
+    println("### process2: wide form > long form transform ###")
+
+    def pivotSummary(desc: DataFrame): DataFrame = {
+      val schema = desc.schema
+      import desc.sparkSession.implicits._
+
+      val longForm = desc.flatMap(row =>{
+        val metric = row.getString(0)
+        (1 until row.size).map(i => {
+          (metric, schema(i).name, row.getString(i).toDouble)
+        })
+      })
+
+      val longDF = longForm.toDF("metric", "field", "value")
+
+      val pivot_values = Seq("count", "mean", "stddev", "min", "max")
+
+      val pivotedDF = longDF
+        .groupBy("field")
+        .pivot("metric", pivot_values)
+        .avg()
+        .sort("field")
+
+      pivotedDF
+    }
+
+
+
+    println("### process3: create a pivot table ###")
+
+    val matchSummaryT = pivotSummary(matchSummary)
+    val missSummaryT = pivotSummary(missSummary)
+
+    matchSummaryT.show()
+    missSummaryT.show()
 
 
 
